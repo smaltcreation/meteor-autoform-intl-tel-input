ee373e31ec0b4ea774513248c71574b00b8b882f
Hazem Khaled <hazem.khaled@gmail.com>
2018-02-14 19:27:32 +0300

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'smaltcreation:autoform-intl-tel-input',
-    version: '1.0.0',
+    version: '2.0.0',
     summary: 'International telephone input for autoform'
 });
 
